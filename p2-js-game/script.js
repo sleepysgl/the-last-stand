f8ee4e61a3cdff246236d4706586b67ffeae8708
Seth Leones <seth.leoness@gmail.com>
2023-02-21 03:20:16 +0800

    Feat+Fix: Added and animated img of enemy, fixed inverted img of enemy.

--- p2-js-game/script.js
+++ p2-js-game/script.js
@@ -14,8 +14,11 @@ let zombies = [];
 class Zombie {
     //Creates one blank object everytime its called with assigned properties
     constructor() {
-        this.width = 100;
-        this.height = 50;
+        //Zombie frame
+        this.zombieWidth = 96;
+        this.zombieHeight = 96;
+        this.width = this.zombieWidth/2;
+        this.height = this.zombieHeight/2;
         //Spawn from right to left 
         this.x = canvas.width;
         //Horizontal spawn position, random number between 0 and canvas height
@@ -24,14 +27,32 @@ class Zombie {
         this.directionX = Math.random() * 0.5 + 0.5;
         //Random vertical speed
         this.directionY = Math.random() * 0.5 - 0.5;
+        this.deleteZombie = false;
+        this.image = new Image();
+        this.image.src = 'Images/Zombie/Walk.png';
+        this.frame = 8;
+        this.maxFrame = 1;
+        this.timeSinceWalk = 0;
+        this.walkInterval = Math.random() * 75 + 75 
     }
-    //Moves zombies around
-    update(){
+    //Moves zombies
+    update(deltatime){
         this.x -= this.directionX;
+        if (this.x < 0 - this.width) this.deleteZombie = true;
+        //Interval of frames per milli-second
+        this.timeSinceWalk += deltatime;
+        if (this.timeSinceWalk > this.walkInterval){
+            if (this.frame < this.maxFrame) this.frame = 4;
+            else this.frame--;
+            this.timeSinceWalk = 0;
+        }
     }
     //Represents a single zombie object
     draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        //source.i, source.x, source.y, source.w, source.h, dest.x, dest.y, dest.w, dest.h)
+        ctx.drawImage(this.image, this.frame * this.zombieWidth, 0, 
+            this.zombieWidth, this.zombieHeight, this.x, this.y, this.width, this.height);
     }
 }
 //Animation loop
@@ -47,9 +68,12 @@ function animate (timestamp){
         timeToNextZombie = 0;
     };
     //Array literals and spread operator(...)
-    //Expands zombies array into another array, spawns zombies periodically
-    [...zombies].forEach(object => object.update());
+    //Cycles thru zombies[] and triggers update()
+    [...zombies].forEach(object => object.update(deltatime));
     [...zombies].forEach(object => object.draw());
+    //Takes zombies[] and deletes it, replaces it with the same contents in zombies[] but only with deleteZombie property set to false
+    zombies = zombies.filter(object => !object.deleteZombie);
+    //console.log(zombies);
     //Creates endless animation loop
     requestAnimationFrame(animate);
 }
